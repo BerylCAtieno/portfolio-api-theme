30c64b10a10bbf1d35e56a10e33b96b8c69a633a
berylatieno <berylatieno30@gmail.com>
2025-09-08 17:51:36 +0300

    make menu responsive

--- api-theme-portfolio/src/components/Header.jsx
+++ api-theme-portfolio/src/components/Header.jsx
@@ -1,20 +1,83 @@
 import { NavLink} from "react-router-dom";
 import { useTheme } from "../context/useTheme.js";
+import { useState } from 'react'
 import '../css/home.css'
 
 function Header() {
 
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     const { toggleTheme } = useTheme();
+
+    const toggleMenu = () => {
+      setIsMenuOpen(!isMenuOpen);
+    };
+
+    const closeMenu = () => {
+      setIsMenuOpen(false);
+    };
 
   return (
     <header className="header">
       {/* TODO: make the nav links responsive for mobile, add "GET /menu" that works like a humburger menu */}
       <nav className="nav">
-        <NavLink to="/" className="endpoint">GET /</NavLink>
-        <NavLink to="/about" className="endpoint">GET /about</NavLink>
-        <NavLink to="/projects" className="endpoint">GET /projects</NavLink>
-        <NavLink to="/opensource" className="endpoint">GET /opensource</NavLink>
-        <NavLink to="/contact" className="endpoint">POST /contact</NavLink>
+        {/* Desktop navigation - hidden on mobile */}
+        <div className="nav-desktop">
+          <NavLink to="/" className="endpoint">GET /</NavLink>
+          <NavLink to="/about" className="endpoint">GET /about</NavLink>
+          <NavLink to="/projects" className="endpoint">GET /projects</NavLink>
+          <NavLink to="/opensource" className="endpoint">GET /opensource</NavLink>
+          <NavLink to="/contact" className="endpoint">POST /contact</NavLink>
+        </div>
+
+        {/* Mobile hamburger menu button */}
+        <div className="nav-mobile">
+          <button 
+            className={`endpoint hamburger-menu ${isMenuOpen ? 'active' : ''}`}
+            onClick={toggleMenu}
+            aria-label="Toggle navigation menu"
+          >
+            GET /menu
+          </button>
+
+          {/* Mobile navigation dropdown */}
+          <div className={`nav-dropdown ${isMenuOpen ? 'open' : ''}`}>
+            <NavLink 
+              to="/" 
+              className="endpoint" 
+              onClick={closeMenu}
+            >
+              GET /
+            </NavLink>
+            <NavLink 
+              to="/about" 
+              className="endpoint" 
+              onClick={closeMenu}
+            >
+              GET /about
+            </NavLink>
+            <NavLink 
+              to="/projects" 
+              className="endpoint" 
+              onClick={closeMenu}
+            >
+              GET /projects
+            </NavLink>
+            <NavLink 
+              to="/opensource" 
+              className="endpoint" 
+              onClick={closeMenu}
+            >
+              GET /opensource
+            </NavLink>
+            <NavLink 
+              to="/contact" 
+              className="endpoint" 
+              onClick={closeMenu}
+            >
+              POST /contact
+            </NavLink>
+          </div>
+        </div>
       </nav>
 
       <div className="theme-toggle">
